2026a5c7b0225c380b416154cf2c45109847ddb2
slopwell <210498326+slopwell@users.noreply.github.com>
2025-07-21 10:19:51 +0900

    feat(fastapi): GET api/aws-service-id/{name} 実装

--- fast-api/src/db/aws_service_dao.py
+++ fast-api/src/db/aws_service_dao.py
@@ -1,6 +1,6 @@
 from typing import Annotated
-from fastapi import Depends, FastAPI, HTTPException, Query
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from fastapi import Depends, HTTPException
+from sqlmodel import Field, Session, SQLModel, select
 from db.core import get_session
 
 SessionDep = Annotated[Session, Depends(get_session)]
@@ -15,23 +15,26 @@ class aws_service(SQLModel, table=True):
     description: str
     category: int = Field(foreign_key="aws_category.name")
 
-def run_query(session: Session, query):
+def run_query(session: Session, query, returns_one: bool = True) :
     """
     SQL実行
     """
-    result = session.exec(query).first()
+    # ScalarResult から値を取り出す必要がある
+    if returns_one:
+        result = session.exec(query).first()
+    else:
+        result = session.exec(query).all()
     if not result:
         raise HTTPException(status_code=404, detail="Item not found")
     return result
 
 
-def get_aws_service_id_sql(name: str, session: SessionDep):
+def get_aws_service_id_sql(name: str, session: SessionDep) -> aws_service:
     """
     ORMによるSQL生成
     """
     sql = select(aws_service).where(aws_service.name == name)
-    result =run_query(session, sql)
+    result = run_query(session, sql)
 
-    print("[get_aws_service_id_sql] result:", result)
     return result
 
